f2cc67a4c7b45a86de67de1945aba1197c882fda
IT22361936 <127702606+IT22361936@users.noreply.github.com>
2023-03-19 09:55:24 +0000

    What did you change

--- .ccls-cache/@home@runner@tutorial-01-IT22361936/Tute03.c
+++ .ccls-cache/@home@runner@tutorial-01-IT22361936/Tute03.c
@@ -12,19 +12,8 @@ n -> 1-
 sum = 1+2+3+...+10 = 55 */
 
 #include <stdio.h>
-int main() 
-{
-  int n, i;
-  int sum;
-
-  printf("Enter the end number : ");
-  scanf("%d", &n);
-
-  for(i = 1; i <= n; i++)
-    {
-      sum += n;
-    }
-  printf("Sum is : %d", sum);
+int main() {
+  printf("Nisitha\n");
   
   return 0;
 }
--- Tute04.c
+++ Tute04.c
@@ -5,15 +5,51 @@ Implement the three functions minimum(), maximum() and multiply() below the main
 Do not change the code given in the main() function when you are implementing your solution.*/
 
 #include <stdio.h>
-
-int main() {
+minimum(int n1, int n2);
+maximum(int n1, int n2);
+multiply(int n1, int n2);
+int main() 
+{
    int no1, no2;
+  
    printf("Enter a value for no 1 : ");
    scanf("%d", &no1);
    printf("Enter a value for no 2 : ");
    scanf("%d", &no2);
-   printf("%d ", minimum(no1, no2));
-   printf("%d ", maximum(no1, no2));
-   printf("%d ", multiply(no1, no2));
+  
+   printf("Minimum number is : %d \n", minimum(no1, no2));
+   printf("Maximum number is : %d \n", maximum(no1, no2));
+   printf("Multiply value is : %d \n", multiply(no1, no2));
    return 0;
+}
+
+minimum(int n1, int n2)
+{
+  int min;
+  
+  if(n1 < n2) {
+    min = n1; }
+  else if (n2 < n1) {
+    min = n2; }
+  
+  return min;
+}
+maximum(int n1, int n2)
+{
+  int max;
+  
+  if(n1 < n2) {
+    max = n2; }
+  else if (n2 < n1) {
+    max = n1; }
+  
+  return max;
+}
+multiply(int n1, int n2)
+{
+  int mltplyl;
+  
+  mltplyl = n1 * n2;
+
+  return mltplyl;
 }
